de98bab09da4eec753019bec820b658ec9c68880
Aadityan R Kurungat <aadityanrkurungat@faustud-010-021-043-028.pool.uni-erlangen.de>
2023-11-16 23:44:16 +0100

    added setup.py, __init__.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dsss_homework_2',
+    version='0.1',
+    packages=['math_quiz'],
+    url='',
+    license='Apache',
+    author='aadityanr',
+    author_email='aadityan.kurungat@fau.de',
+    description=''
+)
